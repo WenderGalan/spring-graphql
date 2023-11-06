3845d7116a1231c31363d9978415e96c686e20cb
Wender Galan <wendergalan2014@hotmail.com>
2023-11-06 17:13:46 -0400

    Finishing graphql client

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,13 +1,15 @@
 <template>
-  <h1>Departamentos</h1>
-  <p v-if="error">Alguma coisa esta errada...</p>
-  <p v-if="loading">Carregando...</p>
-  <p v-else v-for="character in result.departments" :key="character.id">
-    {{ character.name }}
-  </p>
-  <div></div>
+  <div class="bg-blue-400 min-h-screen">
+    <div class="flex items-center justify-center flex-col text-white">
+      <h1 class="text-3xl font-bold my-5">Departamentos</h1>
+      <p v-if="error">Alguma coisa está errada...</p>
+      <p v-if="loading">Carregando...</p>
+      <div v-else v-for="character in result?.departments" :key="character?.id">
+        <p>{{ character?.name }}</p>
+      </div>
+    </div>
+  </div>
 </template>
-
 <script lang="ts">
 import { gql } from '@apollo/client';
 import { useQuery } from '@vue/apollo-composable';
--- client/src/main.ts
+++ client/src/main.ts
@@ -2,6 +2,7 @@ import {createApp, h, provide} from 'vue'
 import App from './App.vue'
 import {DefaultApolloClient} from '@vue/apollo-composable'
 import {ApolloClient, InMemoryCache} from "@apollo/client/core";
+import './index.css'
 
 const apolloClient = new ApolloClient({
     uri: 'http://localhost:8080/graphql',
